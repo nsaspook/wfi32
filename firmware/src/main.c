34bcf2c864f0c507ad8b0b8fdece947f4eb83f79
root <bcddmp>
2022-11-13 18:00:42 -0800

    up

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -109,7 +109,7 @@ imu_cmd_t imu0 = {
 	.op.imu_getid = &sca3300_getid,
 	.op.imu_getdata = &sca3300_getdata,
 	.acc_range = range_15gl,
-	.acc_range_scl = range_inc1,
+	.acc_range_scl = range_inc2,
 	.angles = false,
 };
 #endif
