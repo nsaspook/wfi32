fe263a7af896b3c7ece28d7a0d40b88a57a96d70
Your Name <you@example.com>
2023-02-19 19:58:21 -0800

    up

--- firmware/wfi.X/nbproject/Makefile-variables.mk
+++ firmware/wfi.X/nbproject/Makefile-variables.mk
@@ -6,9 +6,9 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=wfi.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/wfi.X.production.hex
+CND_ARTIFACT_NAME_default=wfi.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/wfi.X.production.null
 # nsensor configuration
 CND_ARTIFACT_DIR_nsensor=dist/nsensor/production
-CND_ARTIFACT_NAME_nsensor=wfi.X.production.hex
-CND_ARTIFACT_PATH_nsensor=dist/nsensor/production/wfi.X.production.hex
+CND_ARTIFACT_NAME_nsensor=wfi.X.production.null
+CND_ARTIFACT_PATH_nsensor=dist/nsensor/production/wfi.X.production.null
